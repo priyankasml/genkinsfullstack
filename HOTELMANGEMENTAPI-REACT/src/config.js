a583c9ba7dd5b5f23ecb177379784a445ae49cb9
unknown <priyankasml54@gmail.com>
2025-10-23 21:42:17 +0530

    Updated files

--- HOTELMANGEMENTAPI-REACT/src/config.js
+++ HOTELMANGEMENTAPI-REACT/src/config.js
@@ -1,2 +1,2 @@
-// Use backend container hostname inside docker network
-export const API_URL = "http://springboot-hoteldb:2000/HOTELMANAGEMENTAPI";
+// For browser access
+export const API_URL = "http://localhost:8081/HOTELMANAGEMENTAPI";
