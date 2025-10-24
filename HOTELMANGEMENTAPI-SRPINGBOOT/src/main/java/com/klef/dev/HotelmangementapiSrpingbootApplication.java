6f311de2276861d8473b50698ed5c640801803c7
unknown <priyankasml54@gmail.com>
2025-10-24 06:34:35 +0530

    Commited

--- HOTELMANGEMENTAPI-SRPINGBOOT/src/main/java/com/klef/dev/HotelmangementapiSrpingbootApplication.java
+++ HOTELMANGEMENTAPI-SRPINGBOOT/src/main/java/com/klef/dev/HotelmangementapiSrpingbootApplication.java
@@ -2,13 +2,17 @@ package com.klef.dev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class HotelmangementapiSrpingbootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HotelmangementapiSrpingbootApplication.class, args);
-		System.out.println("Project running............");
-	}
+    private static final Logger logger = LoggerFactory.getLogger(HotelmangementapiSrpingbootApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelmangementapiSrpingbootApplication.class, args);
+        logger.info("Project running............");
+    }
 
 }
